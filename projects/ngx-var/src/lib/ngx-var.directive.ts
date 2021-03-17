125e4cf59b91c1be3d891955c6e20b15439ac9a3
arndwestermann <58134597+arndwestermann@users.noreply.github.com>
2021-03-17 16:40:28 +0100

    Fixed type not showing correctly in template with Ivy enabled (#2)
    Co-authored-by: Arnd <arnd.westermann@precess.de>

--- projects/ngx-var/src/lib/ngx-var.directive.ts
+++ projects/ngx-var/src/lib/ngx-var.directive.ts
@@ -57,4 +57,30 @@ export class NgxVarDirective<T = unknown> implements OnInit {
   ngOnInit() {
     this._vcr.createEmbeddedView(this._templateRef, this._context);
   }
+
+  /** @internal */
+  public static ngxVarUseIfTypeGuard: void;
+
+  /**
+   * Assert the correct type of the expression bound to the `ngxVar` input within the template.
+   *
+   * The presence of this static field is a signal to the Ivy template type check compiler that
+   * when the `ngxVar` structural directive renders its template, the type of the expression bound
+   * to `ngxVar` should be narrowed in some way. For `ngxVar`, the binding expression itself is used to
+   * narrow its type, which allows the strictNullChecks feature of TypeScript to work with `ngxVar`.
+   */
+  static ngTemplateGuard_ngxVar: 'binding';
+
+  /**
+   * Asserts the correct type of the context for the template that `ngxVar` will render.
+   *
+   * The presence of this method is a signal to the Ivy template type-check compiler that the
+   * `ngxVar` structural directive renders its template with a specific context type.
+   */
+  static ngTemplateContextGuard<T>(
+    dir: NgxVarDirective<T>,
+    ctx: any
+  ): ctx is Context<Exclude<T, false | 0 | '' | null | undefined>> {
+    return true;
+  }
 }
